d27944acefd141bb57dcf15ef8710653f482ad96
ParvizAzeroglu <azerogluparviz@gmail.com>
2024-02-09 14:31:59 +0400

    Pricing page added

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,15 +1,33 @@
 // import React from 'react'
 
 import PageNav from "../components/PageNav";
-
-// type Props = {}
+import styles from "../styles/Pricing.module.css";
 
 const Pricing = () => {
   return (
-    <div>
+    <>
       <PageNav />
-      Pricing
-    </div>
+      <main className={styles.container}>
+        <div className={styles["image-container"]}>
+          <img src="./src/assets/pricing.webp" alt="Pricing webp image" />
+        </div>
+        <div className={styles["text-container"]}>
+          <h1 className="heading">
+            Flexible Pricing Plans for Your Unique Needs
+          </h1>
+          <p className="text">
+            Our pricing plans are designed to suit your budget and business
+            needs. Discover the perfect pricing option that aligns with your
+            requirements and goals. Explore our cost-effective pricing models
+            tailored for your convenience
+          </p>
+          <a href="" className="cta">
+            Contact me
+          </a>
+        </div>
+      </main>
+      <span className="space-60"></span>
+    </>
   );
 };
 
