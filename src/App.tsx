9055b6c16f2f58c27754aa16425499a34e0d4f6b
ParvizAzeroglu <azerogluparviz@gmail.com>
2024-02-12 08:38:26 +0400

    Routing method changed
    reverted react-router-dom-6 router method back to previous method

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,55 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
-import React from "react";
+import { useEffect, useState } from "react";
 import Login from "./pages/Login";
 import AppPage from "./pages/AppPage";
 import CityList from "./components/CityList";
 
-const AppLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="app">{children}</div>;
-};
+const URL = "http://localhost:8000";
 
-const router = createBrowserRouter([
-  {
-    index: true,
-    element: (
-      <AppLayout>
-        <Homepage />
-      </AppLayout>
-    ),
-  },
-  {
-    path: "/pricing",
-    element: (
-      <AppLayout>
-        <Pricing />
-      </AppLayout>
-    ),
-  },
-  {
-    path: "/product",
-    element: (
-      <AppLayout>
-        <Product />
-      </AppLayout>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <AppLayout>
-        <Login />
-      </AppLayout>
-    ),
-  },
-  {
-    path: "/app",
-    element: (
-      <AppLayout>
-        <AppPage />
-      </AppLayout>
-    ),
-    children: [
-      {
-        index: true,
-        element: <CityList />,
-      },
-      {
-        path: "cities",
-        element: <CityList />,
-      },
-      {
-        path: "countries",
-        element: <p>Countries Page</p>,
-      },
-      {
-        path: "form",
-        element: <p>Form</p>,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: (
-      <AppLayout>
-        <PageNotFound />
-      </AppLayout>
-    ),
-  },
-]);
+// const AppLayout = ({ children }: { children: React.ReactNode }) => {
+//   return <div className="app">{children}</div>;
+// };
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  const [cities, setCities] = useState<object>({});
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error fetching data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppPage />}>
+          <Route index element={<CityList />} />
+          <Route path="/cities" element={<CityList />} />
+          <Route path="/countries" element={<p>Countries Page</p>} />
+          <Route path="/form" element={<p>Form Page</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
