eb9d7fd414b04fad178da5196802727042d42993
ParvizAzeroglu <azerogluparviz@gmail.com>
2024-02-14 13:43:57 +0400

    Bug Fixed
    unused variable ignored

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -2,6 +2,8 @@ import { useParams, useSearchParams } from "react-router-dom";
 import styles from "../styles/City.module.css";
 
 const City = () => {
+  // @ts-expect-error: No need to use setSearchParams
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
